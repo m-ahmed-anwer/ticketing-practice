7b900d7c72759edf4df37ef3132806a7b9e24dcd
M.A.Ahmed <ahmedanwer0094@gmail.com>
2024-10-19 06:30:37 +0530

    Initial Commit

---
+++ auth/src/app.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import { json } from "body-parser";
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middleware/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
+import mongoose from "mongoose";
+import cookieSession from "cookie-session";
+import cors from "cors";
+
+const app = express();
+app.set("trust proxy", true);
+app.use(json());
+app.use(cors());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all("*", () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
